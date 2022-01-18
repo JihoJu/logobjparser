db0bbd14f1347d5eea581e726af81c3e0a83d332
JIHOJU <jihoju96@gmail.com>
2022-01-18 15:57:44 +0900

    feat: add patterns module
    Add 2 functions
    - upload_grok_obj
    - upload_regex_obj

---
+++ LogObjParser/handle_pattern.py
@@ -0,0 +1,64 @@
+import re
+from pygrok import Grok
+
+TIME_PATTERN = "%{TIME:time}"
+DATE_PATTERN = "(?<date>%{YEAR}[/-]%{MONTHNUM}[/-]%{MONTHDAY}[T ]|%{MONTH} %{MONTHDAY})"  # Custom Date pattern : 날짜 뒤 T 까지 추출
+URI_PATTERN = "%{URI:url}"
+IP_PATTERN = "(?<ip>%{HOSTNAME}[/:]%{IPV4}([:]%{POSINT})?|[/]%{IPV4}([:]%{POSINT})?|[^-]%{IPV4}([:]%{POSINT})?)"  # 마지막 pattern 은 수정이 필요 : =155.~~ -를 제외한 특수문자를 다 가져옴.
+PATH_PATTERN = "%{PATH:path}"
+
+TIME_GROK = Grok(TIME_PATTERN)
+DATE_GROK = Grok(DATE_PATTERN)
+URI_GROK = Grok(URI_PATTERN)
+IP_GROK = Grok(IP_PATTERN)
+PATH_PATTERN = Grok(PATH_PATTERN)
+
+MASTER_REGEX = re.compile(
+    r"(/?([a-z.A-Z0-9\-_]+/)+[@a-zA-Z0-9\-_+.]+\.[a-zA-Z0-9]{1,10})[:-]?(\d+)?"
+)
+
+MASTER_REGEX_MORE_EXTENSIONS = re.compile(
+    r"(/?([a-z.A-Z0-9\-_]+/)+[@a-zA-Z0-9\-_+.]+\.[a-zA-Z0-9-~]{1,30})[:-]?(\d+)?"
+)
+HOMEDIR_REGEX = re.compile(
+    r"(~/([a-z.A-Z0-9\-_]+/)+[@a-zA-Z0-9\-_+.]+\.[a-zA-Z0-9]{1,10})[:-]?(\d+)?"
+)
+OTHER_BGS_RESULT_REGEX = re.compile(
+    r"(/?([a-z.A-Z0-9\-_]+/)+[a-zA-Z0-9_.]{3,})[:-]?(\d+)"
+)
+ENTIRE_TRIMMED_LINE_IF_NOT_WHITESPACE = re.compile(r"(\S.*\S|\S)")
+JUST_FILE_WITH_NUMBER = re.compile(
+    r"([@%+a-z.A-Z0-9\-_]+\.[a-zA-Z]{1,10})[:-](\d+)(\s|$|:)+"
+)
+JUST_FILE = re.compile(r"([@%+a-z.A-Z0-9\-_]+\.[a-zA-Z]{1,10})(\s|$|:)+")
+JUST_EMACS_TEMP_FILE = re.compile(r"([@%+a-z.A-Z0-9\-_]+\.[a-zA-Z]{1,10}~)(\s|$|:)+")
+JUST_VIM_TEMP_FILE = re.compile(r"(#[@%+a-z.A-Z0-9\-_]+\.[a-zA-Z]{1,10}#)(\s|$|:)+")
+
+
+def upload_grok_obj():
+    """ Returns grok objects after converting them to a dict """
+
+    collection_grok = dict()
+    collection_grok["TIME"] = TIME_GROK
+    collection_grok["DATE"] = DATE_GROK
+    collection_grok["URK"] = URI_GROK
+    collection_grok["IP"] = IP_GROK
+    collection_grok["PATH"] = PATH_PATTERN
+
+    return collection_grok
+
+
+def upload_regex_obj():
+    """ Returns regrex objects after converting them to a dict """
+
+    collection_regex = dict()
+    collection_regex["MASTER_REGEX"] = MASTER_REGEX
+    collection_regex["MASTER_REGEX_MORE_EXTENSIONS"] = MASTER_REGEX_MORE_EXTENSIONS
+    collection_regex["HOMEDIR_REGEX"] = HOMEDIR_REGEX
+    collection_regex["OTHER_BGS_RESULT_REGEX"] = OTHER_BGS_RESULT_REGEX
+    collection_regex["JUST_FILE_WITH_NUMBER"] = JUST_FILE_WITH_NUMBER
+    collection_regex["JUST_FILE"] = JUST_FILE
+    collection_regex["JUST_EMACS_TEMP_FILE"] = JUST_EMACS_TEMP_FILE
+    collection_regex["JUST_VIM_TEMP_FILE"] = JUST_VIM_TEMP_FILE
+
+    return collection_regex
