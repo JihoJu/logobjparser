1841e647c321bc7140e95044b97c3b7e929da2af
JIHOJU <jihoju96@gmail.com>
2022-02-21 23:50:56 +0900

    chore: modify the number of log data

--- LogObjParser/handle_file.py
+++ LogObjParser/handle_file.py
@@ -43,7 +43,7 @@ def extract_log_from_dir(in_dir: str):
     files = get_filenames(in_dir)
     for file in files:
         log_file = open(f"{in_dir}/{file}", "rt")
-        log_lines = log_file.readlines()[:50]
+        log_lines = log_file.readlines()[:1000]
         log_data.extend(log_lines)
 
     return log_data
