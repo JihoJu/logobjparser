0dbca9748b3f31698cb65e62f16e543f12d2117a
JIHOJU <jihoju96@gmail.com>
2022-08-01 14:50:48 +0900

    feat: add extract_log_from_path method
    cli 단에서 명령어로 테스트 시 파일, 디렉토리 여부 상관없이 log 데이터를
    가져올 수 있도록 해당 메서드를 추가

--- Analysis/Path_Analysis.py
+++ Analysis/Path_Analysis.py
@@ -1,3 +1,4 @@
+import os
 from pygrok import Grok
 import LogObjParser.handle_file as hf
 from LogObjParser.pattern import PATH_GROK
@@ -9,7 +10,7 @@ class Path_Analysis:
         self.result = list()
 
     def run(self, path):
-        self.extract_log_from_dir(path)
+        self.extract_log_from_path(path)
         self.identify_existing_file_path_pattern()
         self.identify_custom_file_path_pattern()
         self.compare_result()
@@ -17,12 +18,39 @@ class Path_Analysis:
 
         return 0
 
+    def extract_log_from_path(self, in_path: str):
+        """
+            Extract from the path to log data str object
+        """
+
+        if os.path.isfile(in_path):  # in_path 가 file 인 경우 : 파일 전체 log data
+            self.extract_log_from_file(in_path)
+        elif os.path.isdir(in_path):  # in_path 가 directory 인 경우 : 각 파일 50 줄 log data
+            self.extract_log_from_dir(in_path)
+        else:  # 해당 경로에 파일 혹은 디렉터리가 존재 X
+            print("해당 경로에 파일 혹은 디렉터리가 존재하지 않습니다.")
+
+    def extract_log_from_file(self, in_file: str):
+        """
+            Extract from a file to log data str object
+        """
+        self.result.append(["Where", "Log", "Existing", "Custom"])
+
+        log_file = open(f"{in_file}", "rt")
+        log_lines = log_file.readlines()
+        for log in log_lines:
+            self.result.append([log_file, log])
+
     def extract_log_from_dir(self, in_dir: str):
+        """
+            Extract from files in the directory path to log data str object
+        """
+
         self.result.append(["Where", "Log", "Existing", "Custom"])
         files = hf.get_filenames(in_dir)
         for file in files:
             log_file = open(f"{in_dir}/{file}", "rt")
-            log_lines = log_file.readlines()[:20000]
+            log_lines = log_file.readlines()
             for log in log_lines:
                 self.result.append([file, log])
 
