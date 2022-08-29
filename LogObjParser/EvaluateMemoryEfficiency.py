622455cc84543bcfa8434fdcfc239a37295a9a27
JIHOJU <jihoju96@gmail.com>
2022-08-29 16:15:33 +0900

    feat: add evaluate function to EvaluateMemoryEfficiency.py [#45]
    EvaluateMemoryEfficiency class 제거: 이 class의 여러 instance 를
    생성할 필요가 없다. 또한, 마스킹 기능, 파일간의 메모리 효율성
    측정 기능이 한 class 메서드로 정의하기엔 서로 다른 기능을 한다.
    masking function 을 따로 정의했으며, 두 파일 크기의 감소율을
    측정하는 function 추가.

--- LogObjParser/EvaluateMemoryEfficiency.py
+++ LogObjParser/EvaluateMemoryEfficiency.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool
-from LogObjParser.parser import get_path_objs
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from LogObjParser.parser import get_path_objs
 
 
 def read_data(path: str):
@@ -13,38 +14,38 @@ def read_data(path: str):
             yield log.strip()
 
 
-class EvaluateMemoryEfficiency:
-    """ Evaluate Memory Efficiency Class
+def masking(log: str):
+    """ Masking file path obj to specified obj in a log
 
-    이후에 확장성을 고려해 일단 class 로 구성
+    :param log: A log data
+    :return: 특정 str 객체로 치환된 log data
     """
+    for path_obj in get_path_objs(log):  # file path obj 인식
+        log = log.replace(path_obj, '@path')  # mask_obj: 치환 string 객체
 
-    def __init__(self, mask_obj):
-        self.mask_obj = mask_obj
+    return log + '\n'  # 비효율,,, -> 문자열 객체를 다시 생성하기에,,,
 
-    def masking(self, log: str):
-        """ Masking file path obj to specified obj in a log
 
-        :param log: A log data
-        :return: 특정 str 객체로 치환된 log data
-        """
-        for path_obj in get_path_objs(log):  # file path obj 인식
-            log = log.replace(path_obj, self.mask_obj)  # mask_obj: 치환 string 객체
+def evaluate_memory_efficiency(origin_path, masked_path):
+    """ Evaluate memory efficiency between two files """
 
-        return log + '\n'  # 비효율,,, -> 문자열 객체를 다시 생성하기에,,,
+    original_file = Path(origin_path).stat().st_size
+    masked_file = Path(masked_path).stat().st_size
+
+    return ((masked_file - original_file) / original_file) * 100
 
 
 if __name__ == '__main__':
     data = read_data('../logdata/openstack_clean_seorin.log')
 
-    eme = EvaluateMemoryEfficiency('@path')
-
     # Multi-processing process 개수 8개
     with Pool(8) as p:
-        result = p.map(eme.masking, data)
+        result = p.map(masking, data)
 
     # iter 객체를 파일에 쓰는 IO-bound Task
     # 쓰레드 사용 x: 1.8795 / 멀티 스레드(8) 사용: 0.0323
     with open("../dataset/masked_openstack.log", 'w') as wf:
         with ThreadPoolExecutor(8) as executor:
             executor.map(wf.write, result)
+
+    print(round(evaluate_memory_efficiency('../logdata/openstack_clean_seorin.log', "../dataset/masked_openstack.log"), 4))
